b2720a51cce2000e5734d0d710eaaddc9a306483
cyli <cyli@twistedmatrix.com>
2013-04-10 16:18:49 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="flaschenetikett",
+    version="0.1.0",
+    description="Generates docs from bottle/flask/klein apps",
+    author="cyli",
+    py_modules=["flaschenetikett"],
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+    ]
+)
