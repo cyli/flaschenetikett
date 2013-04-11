d07873808f7e8f4ea607c2b282fa61b72cd39ba6
cyli <cyli@twistedmatrix.com>
2013-04-10 23:44:31 -0700

    Fix bug with setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,29 @@
+import os
 from setuptools import setup
+
+
+def getPackages(base):
+    packages = []
+
+    def visit(arg, directory, files):
+        if '__init__.py' in files:
+            packages.append(directory.replace('/', '.'))
+
+    os.path.walk(base, visit, None)
+
+    return packages
+
 
 setup(
     name="flaschenetikett",
-    version="0.1.0",
+    version='0.0.1',
     description="Generates docs from bottle/flask/klein apps",
-    author="cyli",
-    py_modules=["flaschenetikett"],
     classifiers=[
-        "Programming Language :: Python :: 2.7",
-    ]
+        'Programming Language :: Python :: 2.7',
+    ],
+    maintainer='Ying Li',
+    maintainer_email='cyli@twistedmatrix.com',
+    license='MIT',
+    url='https://github.com/cyli/flaschenetikett/',
+    packages=getPackages('flaschenetikett'),
 )
